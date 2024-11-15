bf87770d0bc400d5557fb17e1fd1ab437f629bd6
vasawat prapachaimongkol <vasawat.ppcmk@gmail.com>
2024-11-15 13:52:10 +0700

    promise

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -12,61 +12,62 @@ router.post("/login", async (req, res) => {
         return
     }
 
-    const user = await User.findOne({email})
+    try {
+        const user = await User.findOne({email})
 
-    if(user){
-        bcrypt.compare(password, user.password, (err, result) =>{
-            if(err) {
-                console.log(err)
-            }
-            if(result){
-                res.status(200).send(user)
-            }else {
-                res.status(404).send("Invalid Password")
-            }
-        })
-    }
-
-})
-
-router.post("/register", (req, res) => {
-    const {email, password} = req.body;
-    if(!email || !password) {
-        res.status(400).send("Email and Password are required")
-        return
-    }
-
-    bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if (err) {
-            console.log(err);
+        if(user){
+            bcrypt.compare(password, user.password, (err, result) =>{
+                if(err) {
+                    console.log(err)
+                }
+                if(result){
+                    res.status(200).send(user)
+                }else {
+                    res.status(404).send("Invalid Password")
+                }
+            })
         }
+    }catch (err) {
+        res.status(500).send({message: "Server error", error: err})
+    }   
+});
+
+router.post("/register", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).send("Email and Password are required");
+    }
+
+    try {
+        const hash = await bcrypt.hash(password, saltRounds);
         const user = await User.create({
             email,
             category: ["food", "transport", "entertainment"],
             password: hash
-        }).then(() => {
-            res.status(201).send(user);
-        }).catch((err) => {
-            res.status(500).send({message: "Server error", error: err})
-        })
-        
-    })
-})
+        });
+
+        res.status(201).send(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ message: "Server error", error: err });
+    }
+});
+
 
 router.delete("/delete", async (req, res) => {
     const {userId} = req.body;
     if(!userId) {
         res.status(404).send("User not found")
         return
-    }else {
-        await User.findByIdAndDelete(userId).then(() => {
-            res.status(200).send("User deleted successfully")
-        }).catch((err) => {
-            res.status(500).send({message: "Server error", error: err})
-        })
     }
 
-    
+    try {
+        await User.findByIdAndDelete(userId)
+        res.status(200).send("User deleted successfully")
+        
+    }catch (err) {
+        res.status(500).send({message: "Server error", error: err})
+    }
 
 })
 
